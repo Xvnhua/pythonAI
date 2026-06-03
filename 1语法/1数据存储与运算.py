381c07906908545b6abb28d5d3bcd82faced018a
86156 <2250966129@qq.com>
2026-06-03 16:10:15 +0800

    第一章

---
+++ 1语法/1数据存储与运算.py
@@ -0,0 +1,20 @@
+# 一、字面量
+
+## 1. Number
+
+# a. int
+a = 131213435446464616515434315343
+# b. float
+b = 3.1452252154545
+
+## 2. bool
+c = True
+d = False
+
+## 3. Str
+e = '人生苦短我用Python'
+
+## 4. NoneType
+f = None
+
+## 5. Vector (List.....)
