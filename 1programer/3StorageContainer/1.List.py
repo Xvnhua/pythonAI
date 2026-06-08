e99ae409b0c59f99920ef2dfef32f161d01ecb4f
86156 <2250966129@qq.com>
2026-06-08 10:30:40 +0800

    List

--- 1programer/3StorageContainer/1.List.py
+++ 1programer/3StorageContainer/1.List.py
@@ -2,6 +2,7 @@
 列表类型是一种 序列类型
 即 可以通过下标访问元素，按照特定顺序排列的序列
 """
+from numpy.ma.extras import average
 
 # 定义
 列表名称 = [1,2,3,4,5,6,7,8,9,10]
@@ -62,3 +63,48 @@ print(a)
 # .reverse() 反转列表元素
 a.reverse()
 print(a)
+
+### 案例 1
+print("接下来输入10个数字")
+int_list1 = []
+aver = 0
+for i in range(10):
+    a = int(input(f'number {i}:'))
+    int_list1.append(a)
+    aver += a
+int_list1.sort()
+print(int_list1)
+aver /=10
+# 直接用sum函数 sum函数接收任何可迭代对象，第二个参数是累加的初始值
+avr2 = sum(int_list1) /10
+
+print("avr2:",avr2)
+print(f'最小值：{int_list1[0]},最大值：{int_list1[-1]}')
+print(f'平均值avr1:{aver}')
+
+# len() 求list 元素个数
+# 也可以用min,max获取最大最小值
+
+### 案例2
+# 合并两个列表中的元素，并进行去重处理
+numblist1 = [19,23,54,64,875,20,109,232,123,54]
+numblist2 = [55,80,72,35,60,123,54,29,91]
+
+numblist3 = []
+for numb in numblist1:
+    if numb not in numblist3:
+        numblist3.append(numb)
+for numb in numblist2:
+    if numb not in numblist3:
+        numblist3.append(numb)
+numblist3.sort()
+print(numblist3)
+
+## 解包+组包操作 用*进行解包操作（解开成单独的元素））
+numblist4 = [*numblist1,*numblist2]
+print(numblist4)
+
+## 加法operator
+numblist5 = numblist1 + numblist2
+
+### 案例3
