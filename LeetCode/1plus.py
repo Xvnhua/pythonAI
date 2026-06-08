50a3ac9b92c1b0fe6ec74bfdd2e94b803917d5a8
Kid of Xiaxia <2250966129@qq.com>
2026-06-08 17:37:50 +0800

    Leetcode第一题

---
+++ LeetCode/1plus.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self,nums, target):
+            """
+            :type nums: List[int]
+            :type target: int
+            :rtype: List[int]
+            """
+            for i in range(len(nums)):
+                if target - nums[i] in nums[i+1:]:
+                    return [i, nums[i+1:].index(target-nums[i])+1]
+
+sol  = Solution()
+result = sol.twoSum([3,3],6)
+print(result)
